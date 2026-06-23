705e94710bfa0c7e6d13c67c2a04933eb62eb1c8
jhoan franco <sebastianfrancogarces1322@gmail.com>
2026-06-23 08:20:08 -0500

    se modifico el app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,10 @@
-let registro = document.getElementById('transacciones');
-fetch('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registro => {
+let transaccion = document.getElementById('transacciones');
+fetch('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registros => {
 
 for(i=0;i<=5;i++){
-registro.innerHTML += `<div class="fila">
-                        <div>${registro.data[i].nombre}</div>
-                        <div>${registro.data[i].monto}</div>
+transaccion.innerHTML += `<div class="fila">
+                        <div>${registros.data[i].nombre}</div>
+                        <div>${registros.data[i].monto}</div>
                     </div>`
 }
 })
