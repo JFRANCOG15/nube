d861a2672024a7aedb52d966349fc675ca35e0f0
jhoan franco <sebastianfrancogarces1322@gmail.com>
2026-06-23 08:08:41 -0500

    se instala cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/router')
+const cross = require('cors')
+
+app.use(cross())
 
 app.listen(3000, ()=> {
     console.log('cree un servidor web')
