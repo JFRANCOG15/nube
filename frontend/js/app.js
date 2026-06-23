41885f6a3e1e1a457ebe4ded424e8ee5e867c778
jhoan franco <sebastianfrancogarces1322@gmail.com>
2026-06-23 09:10:15 -0500

    se modifico el js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,8 @@
-let transaccion = document.getElementById('transacciones');
+let transacciones = document.getElementById('transacciones');
 fetch('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registros => {
 
 for(i=0;i<=5;i++){
-transaccion.innerHTML += `<div class="fila">
+transacciones.innerHTML += `<div class="fila">
                         <div>${registros.data[i].nombre}</div>
                         <div>${registros.data[i].monto}</div>
                     </div>`
