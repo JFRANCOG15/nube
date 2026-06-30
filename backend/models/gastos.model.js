ea552040119c736b363930d00856b17ff8439b13
jhoan franco <sebastianfrancogarces1322@gmail.com>
2026-06-30 08:07:22 -0500

    se actualizado el model/gastos

--- backend/models/gastos.model.js
+++ backend/models/gastos.model.js
@@ -3,7 +3,7 @@ const db = require('../config/db')
 class GastosModel{
     static async obtenerGastos(){
         const [rows] = await db.query(
-            'SELECT ct.nombre, ts.monto, ts.tipo, ts.fecha FROM transacciones ts INNER JOIN categoria ct ON ts.ct_id = ct.id LIMIT 5'
+            'SELECT ct.nombre, ts.monto, ts.tipo, ts.fecha FROM transacciones ts INNER JOIN categoria ct ON ts.ct_id =  LIMIT 5'
         )
         return rows
     }
